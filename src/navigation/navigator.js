17256f58c70712d59fe1b6dabc7f537402ce45ed
Waqar Qureshi <waqarqureshi90@gmail.com>
2021-04-02 16:04:23 +0500

    directory structure changed

---
+++ src/navigation/navigator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Notes from '../screens/notesScreen';
+
+const Navigator = () => {
+  return (
+    <>
+      <Notes />
+    </>
+  );
+};
+
+export default Navigator;
---
+++ src/screens/notesScreen/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './style.js';
+
+const Notes = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>notes app</Text>
+      </View>
+    </>
+  );
+};
+
+export default Notes;
