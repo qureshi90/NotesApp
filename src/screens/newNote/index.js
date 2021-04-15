744fe3cdbf03ded3162b945631810423677c0bf8
Waqar Qureshi <waqarqureshi90@gmail.com>
2021-04-15 14:42:57 +0500

    check and bullet can ce added from popup menu

--- src/screens/newNote/index.js
+++ src/screens/newNote/index.js
@@ -9,11 +9,19 @@ import {
 import styles from './style.js';
 import {Icon} from 'react-native-elements';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
 
 const NewNote = ({navigation, route}) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [notes, setNotes] = useState([]);
+  const [check, setCheck] = useState(false);
+  const [bullet, setBullet] = useState(false);
 
   let index = route.params.index;
 
@@ -24,6 +32,8 @@ const NewNote = ({navigation, route}) => {
         setNotes(JSON.parse(data));
         setTitle(notes[index].title);
         setDescription(notes[index].description.join('\n'));
+        setCheck(notes[index].checkStatus);
+        setBullet(notes[index].bullet);
       }
     })();
   }, [description, index, notes, title]);
@@ -33,8 +43,8 @@ const NewNote = ({navigation, route}) => {
       if (!isNaN(index)) {
         notes[index] = {
           title: title,
-          // checkStatus: check,
-          // bullet: bullet,
+          checkStatus: check,
+          bullet: bullet,
           description: description.split('\n'),
         };
         try {
@@ -46,8 +56,8 @@ const NewNote = ({navigation, route}) => {
       } else {
         notes.unshift({
           title: title,
-          // checkStatus: check,
-          // bullet: bullet,
+          checkStatus: check,
+          bullet: bullet,
           description: description.split('\n'),
         });
         try {
@@ -93,6 +103,34 @@ const NewNote = ({navigation, route}) => {
           onChangeText={text => setDescription(text)}
         />
       </ScrollView>
+      <View style={styles.fabButtonStyle}>
+        <Menu>
+          <MenuTrigger>
+            <Icon
+              type={'feather'}
+              name="more-vertical"
+              size={26}
+              color="#fff"
+            />
+          </MenuTrigger>
+          <MenuOptions>
+            <MenuOption
+              onSelect={() => {
+                setCheck(!check);
+                setBullet(false);
+              }}>
+              {check ? <Text>Remove Check</Text> : <Text>Add Check</Text>}
+            </MenuOption>
+            <MenuOption
+              onSelect={() => {
+                setBullet(!bullet);
+                setCheck(false);
+              }}>
+              {bullet ? <Text>Remove Bullets</Text> : <Text>Add Bullets</Text>}
+            </MenuOption>
+          </MenuOptions>
+        </Menu>
+      </View>
     </View>
   );
 };
